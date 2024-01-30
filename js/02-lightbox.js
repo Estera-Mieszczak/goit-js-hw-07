5c9817cf3ff4656bb3a29de47e70bad74da2b819
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-01-30 20:36:44 +0100

    Update 02-lightbox.html 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryCard = galleryItems.map(
+    (item) => `<li><a class = "gallery__item" href = "${item.original}"><img class = "gallery__image" src = "${item.preview}" alt = "${item.description}"/></a></li>`
+).join("");
 
-console.log(galleryItems);
+gallery.innerHTML = galleryCard;
+
+const lightbox = new SimpleLightbox('.gallery, a', {
+    captions: true,
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
